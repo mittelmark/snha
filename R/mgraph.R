27c7947b533e6bec401393b0e421672bb4de3bfb
Detlef Groth <detlef.groth@uni-potsdam.de>
2023-06-10 10:06:50 +0200

    adding functions u2d and nodeColors

--- R/mgraph.R
+++ R/mgraph.R
@@ -185,6 +185,7 @@ mgraph <- function (x=NULL,type="random",mode="directed",nodes=10,edges=12,m=1,k
     return(x)
     
 }
+plot.mgraph=plot.snha
 
 #' \name{mgraph_autonames}
 #' \alias{mgraph_autonames}
@@ -289,3 +290,212 @@ mgraph_d2u <- function (g) {
     return(g)
 }
 
+#' \name{mgraph_nodeColors}
+#' \alias{mgraph_nodeColors}
+#' \title{create node colors for directed graphs}
+#' \description{
+#'   This function simplifies automatic color coding of nodes for directed graphs.
+#'   Nodes will be colored based on their degree properties, based
+#'   on their incoming and outcoming edges.
+#' }
+#' \usage{mgraph_nodeColors(g,
+#'     col=c("skyblue","grey80","salmon")
+#' )}
+#' \arguments{
+#' \item{g}{a mgraph object or an adjacency matrix or an adjacency list}
+#' \item{col}{default colors for nodes with only incoming, in- and outgoing and only outgoing edges, default: c("skyblue","grey80","salmon")}
+#  }
+#' \examples{
+#' par(mfrow=c(1,2),mai=rep(0.1,4))
+#' A=mgraph(type="random",nodes=6,edges=8)
+#' cols=mgraph_nodeColors(A)
+#' mgraph_degree(A,mode="in")
+#' mgraph_degree(A,mode="out")
+#' cols
+#' plot(A, layout="star")
+#' plot(A, layout="star",vertex.color=cols) 
+#' }
+#'
+
+mgraph_nodeColors <- function (g,col=c("skyblue","grey80","salmon")) {
+    colors = rep(col[2],nrow(g))
+    out    = mgraph_degree(g,mode="out")
+    inc     = mgraph_degree(g,mode="in")    
+    colors[out >  0 & inc == 0] = col[3]
+    colors[out == 0 & inc >  0] = col[1]    
+    return(colors)
+}
+
+#' \name{mgraph_u2d}
+#' \alias{mgraph_u2d}
+#' \title{reate a directed graph out of an undirected graph}
+#' \description{
+#'   This function creates a directed graph from an undirected one by the given input nodes. Input nodes can be chosen by names or a number for random selection of input nodes will be given.
+#' Input nodes will have at least shortest path distance to other input nodes of pathlength two.
+#' Selected input nodes will draw in each iteration outgoing edges to other nodes in the nth iteration neighborhood. The input
+#' nodes will alternatively select the next edges on the path to not visited nodes. All edges will be only visited onces.
+#' }
+#' \usage{mgraph_u2d(
+#'     g,input=2,negative=0.0,shuffle=FALSE
+#' )}
+#' \arguments{
+#' \item{g}{mgraph object created with mgraph}
+#' \item{input}{number or names of input nodes in the graph, if number of input nodes is smaller than number of components, for each component one input node is automatically created}
+#' \item{negative}{proportion of inhibitive associations in the network value between 0 and 1 are acceptable, Default 0.0}
+#' \item{shuffle}{should just the edge directions beeing shuffled, if TRUE the graph will be very random without a real structure or chains of associations, default: FALSE}
+#  }
+#' \examples{
+#' par(mfrow=c(2,2),mai=rep(0.1,4))
+#' G=mgraph(type="angie",nodes=7,edges=9)
+#' mgraph_degree(G,mode='in')
+#' U=mgraph_d2u(G)
+#' H=mgraph_u2d(U,input=c("G","E"))
+#' identical(G,H)
+#' I=mgraph_u2d(U,shuffle=TRUE)
+#' identical(G,I)
+#' lay=snha_layout(G)
+#' plot(G,layout=lay,vertex.color=mgraph_nodeColors(G))
+#' plot(U,layout=lay)
+#' plot(H,layout=lay,vertex.color=mgraph_nodeColors(H))
+#' plot(I,layout=lay,vertex.color=mgraph_nodeColors(I))
+#' }
+#' 
+
+mgraph_u2d <- function (g,input=2,negative=0.0,shuffle=FALSE) {
+    if (shuffle) {
+        h=g
+        idx=sample(rownames(h))
+        h=h[idx,idx]
+        h[lower.tri(h)]=0
+        h=h[rownames(g),rownames(g)]
+        class(h)="mgraph"
+        return(h)          
+    } else {
+        A=g
+        # creates from undirected a directed one
+        # A must be an symmetric adjacency matrix
+        # MN The new check is 5x faster and takes less memory
+        #if (!identical(A[lower.tri(A)], t(A)[lower.tri(A)])) {
+        if (!all(A == t(A))) {
+            stop("adjacency matrix must be symmetric")
+        }
+        if (negative < 0 || negative > 1) {
+            stop("negative proportions must be within 0 and 1")
+        }
+        # undirected matrix
+        U=A
+        # future directed matrix
+        D=U
+        D[]=0
+        neighbours=c()
+        if (class(input)=='numeric') {
+            nodes=c()
+            
+            comps=Mgraph_components(A)
+            if (max(comps)>1) {
+                for (i in 1:max(comps)) {
+                    if (length(which(comps==i)) > 1) {
+                        node=sample(names(which(comps==i)),1)
+                        neighbours=c(neighbours,rownames(U)[which(U[node,]==1)],node)
+                        nodes=c(nodes,node)
+                    }
+                }
+            }
+            n=input-length(nodes)
+            while (n>0) {
+                rnames=setdiff(rownames(U),c(neighbours))
+                node=sample(rnames,1)
+                n=n-1
+                neighbours=c(neighbours,rownames(U)[which(U[node,]==1)],node)
+                nodes=c(nodes,node)
+            }
+        } else {
+            nodes=input
+            n=0
+        }
+        visits=list()
+        while (length(nodes)>0) {
+            node=nodes[1]
+            edges=which(U[node,]==1)
+            newnodes=colnames(U)[edges]
+            if (length(nodes)==1) {
+                nodes=newnodes
+            } else {
+                nodes=c(nodes[2:length(nodes)],newnodes)
+            }
+            D[node,edges]=1
+            U[node,]=0
+            U[,node]=0
+        }
+        A[]=D
+        if (negative>0) {
+            idx=which(A==1)
+            n=floor(length(idx)*negative)
+            if(n>0) {
+                min=sample(idx,n)
+                A[min]=-1
+            }
+        }
+        class(A)="mgraph"
+        return(A)
+    }
+}
+
+
+Mgraph_components <- function (g) {
+    A=g
+    A=as.matrix(A)
+    A=A+t(A)
+    A[A>0]=1
+    comp=c()
+    P=Mgraph_shortest.paths(A)
+    nodes=rownames(A)
+    x=1
+    while (length(nodes) > 0) {
+        n=nodes[1]
+        idx=which(P[n,] < Inf)
+        ncomp=rep(x,length(idx))
+        names(ncomp)=rownames(P)[idx]
+        comp=c(comp,ncomp)
+        nodes=setdiff(nodes,rownames(P)[idx])
+        x=x+1
+    }
+    return(comp[rownames(A)])
+}
+
+Mgraph_shortest.paths <- function (g,mode="directed") {
+    A=g
+    if (mode == "undirected") {
+        A=A+t(A)
+        A[A!=0]=1
+    }
+    S=A
+    S[]=Inf
+    diag(S)=0
+    x=1
+    S[A > 0 & A < Inf]=1
+    while (TRUE) { 
+        flag = FALSE 
+        for (m in 1:nrow(S)) {
+            ns=which(S[m,] == x)
+            for (n in ns) {
+                for (o in which(A[n,]==1)) {
+                    if (o != m) {
+                        flag = TRUE
+                        if (S[m,o] > x + 1) {
+                            S[m,o]=x+1
+                            if (mode == "undirected") {
+                                S[o,m]=x+1
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        if (!flag) {
+            break
+        }
+        x=x+1
+    }
+    return(S)
+}
