e2cfda92d6530b3ef7e9a46dd12ef208abf5bcec
Detlef Groth <detlef.groth@uni-potsdam.de>
2023-06-28 08:43:06 +0200

    benchmark extended for method of packages huge, glmnet and qgraph

--- bin/benchmark.R
+++ bin/benchmark.R
@@ -1,6 +1,89 @@
 library(snha)
 library(lattice)
 benchmark <-  function (iter=3) {
+    huge.predict <- function (x,method="mb",criterion="stars") {
+        if (!requireNamespace("huge", quietly = TRUE)) {
+            stop("Package \"huge\" needed for this function to work. Please install it.",
+                 call. = FALSE)
+        }
+        library(huge)
+        out.mb = huge(x,method=method,verbose=FALSE)
+        M=as.matrix(huge.select(out.mb,criterion=criterion,verbose=FALSE)$refit)
+        rownames(M)=colnames(M)=colnames(x)
+        G=list()
+        G$theta=M
+        return(G)
+    }   
+    qgraph.predict <- function (x,type="ebic") {
+        if (!requireNamespace("qgraph", quietly = TRUE)) {
+            stop("Package \"qgraph\" needed for this function to work. Please install it.",
+                 call. = FALSE)
+        }
+        require("qgraph")
+        C=cor(x)
+        if (type=="ebic") {
+            res=EBICglasso(C,n=nrow(x),0.5,threshold=TRUE)
+        } else if (type=="bic") {
+            res=EBICglasso(C,n=nrow(x),0,threshold=TRUE)
+        } else {
+            stop("Error: type must be either 'ebic' or 'bic'")
+        }
+        res[res!=0]=1
+        res[res==1 & C < 0]=-1
+        
+        return(list(theta=res))
+    }
+    glmnet.predict <- function (x,type="lasso",rs=0.04,alpha=1) {
+        if (!requireNamespace("glmnet", quietly = TRUE)) {
+            stop("Package \"glment\" needed for this function to work. Please install it.",
+                 call. = FALSE)
+        }
+        if (is.data.frame(x)) {
+            x=as.matrix(x)
+        }
+        A=matrix(0,nrow=ncol(x),ncol=ncol(x))
+        rownames(A)=colnames(A)=colnames(x)
+        Coef=A
+        rsqs=c()
+        for (i in 1:ncol(x)) {
+            xi=x[,-i]
+            yi=x[,i]
+            CV=glmnet::cv.glmnet(as.matrix(xi),yi,alpha=alpha,family="gaussian")
+            lasso.model <- glmnet::glmnet(as.matrix(xi),yi, family = "gaussian",
+                                          alpha=alpha,  lambda = CV$lambda.1se )
+            rsqs=c(rsqs,lasso.model$dev.ratio)
+            # normalize coefficients Agresti method(?)
+            sds=apply(as.matrix(xi),2,sd)
+            cs = as.matrix(coef(lasso.model, s = "lambda.1se"))
+            std.coef = cs[-1, 1] * sds
+            Coef[i,names(std.coef)]=std.coef
+            c=coef(CV)[,1]
+            c=c[2:length(c)]
+            nm=names(which(c!=0))
+            A[i,nm]=c[nm]
+        }
+        names(rsqs)=colnames(x)
+        rsq=(abs(Coef)/apply(abs(Coef),1,sum))*rsqs
+        C=A
+        A[rsq<rs]=0
+        A[A<0]=-1
+        A[A>0]=1
+        if (alpha == 1) {
+            type="lasso"
+        } else if (alpha == 0) {
+            type="ridge"
+        } else {
+            type="elnet"
+        }
+        G=list()
+        G$theta=A
+        G$type=type
+        G$coef=C
+        G$std.coef=Coef
+        G$r.squared=rsq
+        return(G)
+    }
+    
     performance <-  function (graph,data,FUN, ...) {
         t1=Sys.time()
         pred=FUN(data,...) 
@@ -25,6 +108,32 @@ benchmark <-  function (iter=3) {
         res=performance(graph,g.data,snha,lmc=TRUE,prob=TRUE)
         data=rbind(data,
                    data.frame(method="5lmb",t(unlist(res))))
+        # glmnet
+        res=performance(graph,g.data,glmnet.predict)
+        data=rbind(data,
+                   data.frame(method="g.ls",t(unlist(res))))
+        # huge criterion ric does  not work!!
+        res=performance(graph,g.data,huge.predict,method="glasso",criterion="stars")
+        data=rbind(data,
+                   data.frame(method="h.ls.s",t(unlist(res))))
+        #res=performance(graph,g.data,huge.predict,method="mb",criterion="stars")
+        #data=rbind(data,
+        #           data.frame(method="h.mb.s",t(unlist(res))))
+        res=performance(graph,g.data,huge.predict,method="ct",criterion="stars")
+        data=rbind(data,
+                   data.frame(method="h.ct.s",t(unlist(res))))
+        res=performance(graph,g.data,huge.predict,method="glasso",criterion="ebic")
+        data=rbind(data,
+                   data.frame(method="h.ls.e",t(unlist(res))))
+        res=performance(graph,g.data,huge.predict,method="glasso",criterion="ric")
+        data=rbind(data,
+                   data.frame(method="h.ls.r",t(unlist(res))))
+        res=performance(graph,g.data,qgraph.predict,type="ebic")
+        data=rbind(data,
+                   data.frame(method="q.ls.e",t(unlist(res))))
+        res=performance(graph,g.data,qgraph.predict,type="bic")
+        data=rbind(data,
+                   data.frame(method="q.ls.b",t(unlist(res))))
         return(data)
     }
     W=mgraph(type="werner")
